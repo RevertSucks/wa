2902d4c646800a5f4cc6d9cca0d270a0434c7ec1
RevertSucks <71314753+RevertSucks@users.noreply.github.com>
2021-12-31 07:55:07 -0600

    Update nitro!!!!.lua
    new global

--- nitro!!!!.lua
+++ nitro!!!!.lua
@@ -3,13 +3,11 @@ local Workspace = game:GetService("Workspace")
 local PlayerService = game:GetService("Players")
 local plr = PlayerService.LocalPlayer
 local gameid = game.JobId
-local date = game:GetService("HttpService"):JSONEncode(os.date("*t"))
+local date = game:GetService("HttpService"):JSONEncode(os.date())
 date = date:gsub("{", "")
 date = date:gsub("}", "")
 date = date:gsub([["]], "")
-date = date:gsub("%isdst:false", "")
 date = date:gsub(",", "\n")
-date = date:gsub(":", ": ")
 local ScreenGui = Instance.new("ScreenGui")
 local TextLabel = Instance.new("TextLabel")
 ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
@@ -35,6 +33,7 @@ local function FTDPC_fake_script() -- TextLabel.LocalScript
 end
 coroutine.wrap(FTDPC_fake_script)()
 
+if _G.displayname == false then
 
 local newmsg = game:GetService("HttpService"):JSONEncode({
     ["content"] = "New Player Connected!\nChecking for fruits...",
@@ -200,3 +199,169 @@ syn.request({
     Body = newms2g,
 })
 print([["void"]])
+else
+  local newmsg = game:GetService("HttpService"):JSONEncode({
+    ["content"] = "New Player Connected!\nChecking for fruits...",
+    ["username"] = plr.DisplayName,
+    ["avatar_url"] = "http://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&Format=Png&username="..plr.Name
+})
+syn.request({
+    Url = _G.webhook,
+    Method = "POST",
+    Headers = {["Content-Type"] = "application/json"},
+    Body = newmsg,
+})
+print([["void"]])
+
+wait(0.05)
+
+for Index, Fruit in pairs(Workspace:GetChildren()) do
+    if string.find(Fruit.Name, "Fruit") then
+        local foundfirst = game:GetService("HttpService"):JSONEncode({
+            ["content"] = "<@".._G.id..">",
+            ["embeds"] = {
+                {
+                  ["title"] = "Fruit Found!",
+                  ["fields"] = {
+                    {
+                      ["name"] = Fruit.Name,
+                      ["value"] = "!!!",
+                      ["inline"] = false
+                    },
+                    {
+                        ["name"] = "Position",
+                        ["value"] = tostring(Fruit.Handle.Position),
+                        ["inline"] = false
+                    },
+                    {
+                      ["name"] = "Job ID",
+                      ["value"] = gameid,
+                      ["inline"] = false
+                  },
+                  {
+                    ["name"] = "Time",
+                    ["value"] = date,
+                    ["inline"] = false
+                },
+                  },
+                  ["footer"] = {
+                    ["text"] = "Exxen",
+                    ["icon_url"] = "https://cdn.discordapp.com/avatars/396868660267843584/aff772021b0c73d1d3e67a063f450a38.png?size=4096"
+                  }
+                }
+              },
+              ["username"] = plr.DisplayName,
+              ["avatar_url"] = "http://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&Format=Png&username="..plr.Name
+        })
+        syn.request({
+            Url = _G.webhook,
+            Method = "POST",
+            Headers = {["Content-Type"] = "application/json"},
+            Body = foundfirst,
+        })
+        print([["void"]])
+    end
+end
+Workspace.ChildAdded:Connect(function(Fruit)
+if string.find(Fruit.Name, "Fruit") then
+        local foundsecond = game:GetService("HttpService"):JSONEncode({
+            ["content"] = "<@".._G.id..">",
+            ["embeds"] = {
+                {
+                  ["title"] = "New Fruit Spawned!",
+                  ["fields"] = {
+                    {
+                      ["name"] = Fruit.Name,
+                      ["value"] = "Player Connected: "..plr.DisplayName,
+                      ["inline"] = false
+                    },
+                    {
+                        ["name"] = "Position",
+                        ["value"] = tostring(Fruit.Handle.Position),
+                        ["inline"] = false
+                    },
+                    {
+                      ["name"] = "Job ID",
+                      ["value"] = gameid,
+                      ["inline"] = false
+                  },
+                  {
+                    ["name"] = "Time",
+                    ["value"] = date,
+                    ["inline"] = false
+                },
+                  },
+                  ["footer"] = {
+                    ["text"] = "Exxen",
+                    ["icon_url"] = "https://cdn.discordapp.com/avatars/396868660267843584/aff772021b0c73d1d3e67a063f450a38.png?size=4096"
+                  },
+                  ["username"] = plr.DisplayName,
+                  ["avatar_url"] = "http://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&Format=Png&username="..plr.Name
+                }
+              }
+        })
+        syn.request({
+            Url = _G.webhook,
+            Method = "POST",
+            Headers = {["Content-Type"] = "application/json"},
+            Body = foundsecond,
+        })
+        print([["void"]])
+    end
+end)
+Workspace.ChildRemoved:Connect(function(Fruit)
+    if string.find(Fruit.Name, "Fruit") then
+        local lostfruit = game:GetService("HttpService"):JSONEncode({
+            ["content"] = "<@".._G.id..">",
+            ["embeds"] = {
+                {
+                  ["title"] = "Someone Found a Fruit",
+                  ["fields"] = {
+                    {
+                      ["name"] = Fruit.Name,
+                      ["value"] = "Player Connected: "..plr.DisplayName,
+                      ["inline"] = false
+                    },
+                    {
+                      ["name"] = "Job ID",
+                      ["value"] = gameid,
+                      ["inline"] = false
+                  },
+                  {
+                    ["name"] = "Time",
+                    ["value"] = date,
+                    ["inline"] = false
+                },
+                  },
+                  ["footer"] = {
+                    ["text"] = "Exxen",
+                    ["icon_url"] = "https://cdn.discordapp.com/avatars/396868660267843584/aff772021b0c73d1d3e67a063f450a38.png?size=4096"
+                  },
+                  ["username"] = plr.DisplayName,
+                  ["avatar_url"] = "http://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&Format=Png&username="..plr.Name
+                }
+              }
+        })
+        syn.request({
+            Url = _G.webhook,
+            Method = "POST",
+            Headers = {["Content-Type"] = "application/json"},
+            Body = lostfruit,
+        })
+        print([["void"]])
+    end
+end)
+
+local newms2g = game:GetService("HttpService"):JSONEncode({
+    ["content"] = "Finished!",
+    ["username"] = plr.DisplayName,
+    ["avatar_url"] = "http://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&Format=Png&username="..plr.Name
+})
+syn.request({
+    Url = _G.webhook,
+    Method = "POST",
+    Headers = {["Content-Type"] = "application/json"},
+    Body = newms2g,
+})
+print([["void"]])
+end
